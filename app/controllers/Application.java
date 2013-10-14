c45a01a7c66bed1e379d666a9604e862045b99ab
jortal <jortal@hawaii.edu>
2013-10-13 18:58:56 -1000

    Attempt-1 6:22pm

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,7 +3,7 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.Index;
-import views.html.Page1;
+import views.html.NewContact;
 
 /**
  * Implements the controllers for this application.
@@ -22,8 +22,8 @@ public class Application extends Controller {
    * Returns page1, a simple example of a second page to illustrate navigation.
    * @return The Page1.
    */
-  public static Result page1() {
-    return ok(Page1.render("Welcome to Page1."));
+  public static Result newcontact() {
+    return ok(NewContact.render("Welcome to Page1."));
     
   }
 }
