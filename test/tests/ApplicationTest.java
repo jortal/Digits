fc50ecd9f0fd29db15d266d1dfd3745a2621df2a
jortal <jortal@hawaii.edu>
2013-12-09 07:58:55 -1000

    Starting WOD

--- test/tests/ApplicationTest.java
+++ test/tests/ApplicationTest.java
@@ -1,4 +1,4 @@
-package test;
+package tests;
 
 import org.junit.Test;
 import static org.fest.assertions.Assertions.assertThat;
--- test/tests/IntegrationTest.java
+++ test/tests/IntegrationTest.java
@@ -1,4 +1,4 @@
-package test;
+package tests;
 
 import org.junit.Test;
 import play.test.TestBrowser;
